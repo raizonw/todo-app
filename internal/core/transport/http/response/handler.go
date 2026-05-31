25555a654c23e678d2684c3c10f7fad4a66316da
raizonw <i.dmitriev781@gmail.com>
2026-06-01 01:34:24 +0300

    decode and validate JSON

--- internal/core/transport/http/response/handler.go
+++ internal/core/transport/http/response/handler.go
@@ -2,9 +2,11 @@ package core_http_response
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
+	core_errors "github.com/raizonw/todo-app/internal/core/errors"
 	core_logger "github.com/raizonw/todo-app/internal/core/logger"
 	"go.uber.org/zap"
 )
@@ -29,6 +31,47 @@ func (h *HTTPResponseHandler) PanicResponse(p any, msg string) {
 	err := fmt.Errorf("unexpected panic: %v", p)
 
 	h.log.Error(msg, zap.Error(err))
+	h.errorResponse(statusCode, err, msg)
+}
+
+func (h *HTTPResponseHandler) ErrorResponse(err error, msg string) {
+	var (
+		statusCode int
+		logFunc    func(string, ...zap.Field)
+	)
+
+	switch {
+	case errors.Is(err, core_errors.ErrInvalidArgument):
+		statusCode = http.StatusBadRequest
+		logFunc = h.log.Warn
+
+	case errors.Is(err, core_errors.ErrNotFound):
+		statusCode = http.StatusNotFound
+		logFunc = h.log.Debug
+
+	case errors.Is(err, core_errors.ErrConflict):
+		statusCode = http.StatusConflict
+		logFunc = h.log.Warn
+
+	default:
+		statusCode = http.StatusInternalServerError
+		logFunc = h.log.Error
+	}
+
+	logFunc(msg, zap.Error(err))
+
+	h.errorResponse(
+		statusCode,
+		err,
+		msg,
+	)
+}
+
+func (h *HTTPResponseHandler) errorResponse(
+	statusCode int,
+	err error,
+	msg string,
+) {
 	h.rw.WriteHeader(statusCode)
 
 	response := map[string]string{
