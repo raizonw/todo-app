6fcab26e3c4ec579a59fa9c994564581c2065fe7
raizonw <i.dmitriev781@gmail.com>
2026-06-01 00:08:45 +0300

    chain middleware and simple app start

--- internal/features/users/transport/http/create_user.go
+++ internal/features/users/transport/http/create_user.go
@@ -24,10 +24,12 @@ func (h *UsersHTTPHandler) CreateUser(rw http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log := core_logger.FromContext(ctx)
 
-	log.Debug("invoce CreateUser handler")
+	log.Debug("invoke CreateUser handler")
 
 	var request CreateUserRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		fmt.Println("Unexpected error while creating user")
 	}
+
+	rw.WriteHeader(http.StatusOK)
 }
