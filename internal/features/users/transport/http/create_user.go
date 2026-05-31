7ed8d56cd1ecfbaf248f5585fb730c71c8899d3b
raizonw <i.dmitriev781@gmail.com>
2026-06-01 01:34:24 +0300

    decode and validate JSON

--- internal/features/users/transport/http/create_user.go
+++ internal/features/users/transport/http/create_user.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 
 	core_logger "github.com/raizonw/todo-app/internal/core/logger"
+	core_http_request "github.com/raizonw/todo-app/internal/core/transport/http/request"
+	core_http_response "github.com/raizonw/todo-app/internal/core/transport/http/response"
 )
 
 type CreateUserRequest struct {
-	FullName    string  `json:"full_name"`
-	PhoneNumber *string `json:"phone_number"`
+	FullName    string  `json:"full_name" validate:"required,min=3,max=100"`
+	PhoneNumber *string `json:"phone_number" validate:"omitempry,e164"`
 }
 
 type CreateUserResponse struct {
@@ -23,10 +25,16 @@ type CreateUserResponse struct {
 func (h *UsersHTTPHandler) CreateUser(rw http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log := core_logger.FromContext(ctx)
+	responseHandler := core_http_response.NewHTTPResponseHandler(log, rw)
 
 	log.Debug("invoke CreateUser handler")
 
 	var request CreateUserRequest
+	if err := core_http_request.DecodeAndValidateRequest(r, &request); err != nil {
+		responseHandler.ErrorResponse(err, "failed to decode and validate HTTP request")
+
+		return
+	}
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		fmt.Println("Unexpected error while creating user")
 	}
