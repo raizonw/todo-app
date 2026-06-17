562969acfada72d713273f8e90fdc8b094123093
raizonw <i.dmitriev781@gmail.com>
2026-06-17 20:16:55 +0300

    feat(tasks): add task creation API endpoint

---
+++ internal/features/tasks/repository/postgres/create_task.go
@@ -0,0 +1,80 @@
+package tasks_postgres_repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/raizonw/todo-app/internal/core/domain"
+	core_errors "github.com/raizonw/todo-app/internal/core/errors"
+)
+
+const pgxViolatesForeignKeyErrorKey = "23503"
+
+func (r *TasksRepository) CreateTask(
+	ctx context.Context,
+	task domain.Task,
+) (domain.Task, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+	INSERT INTO todoapp.tasks (title, description, completed, created_at, completed_at, author_user_id)
+	VALUES ($1, $2, $3, $4, $5, $6)
+	RETURNING id, version, title, description, completed, created_at, completed_at, author_user_id;
+	`
+
+	row := r.pool.QueryRow(
+		ctx,
+		query,
+		task.Title,
+		task.Description,
+		task.Completed,
+		task.CreatedAt,
+		task.CompletedAt,
+		task.AuthorUserID,
+	)
+
+	var taskModel TaskModel
+
+	err := row.Scan(
+		&taskModel.ID,
+		&taskModel.Version,
+		&taskModel.Title,
+		&taskModel.Description,
+		&taskModel.Completed,
+		&taskModel.Created_At,
+		&taskModel.Completed_At,
+		&taskModel.AuthorUserID,
+	)
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == pgxViolatesForeignKeyErrorKey {
+				return domain.Task{}, fmt.Errorf(
+					"%v: user with id = `%d`: %w",
+					err,
+					task.AuthorUserID,
+					core_errors.ErrNotFound,
+				)
+			}
+		}
+
+		return domain.Task{}, fmt.Errorf("scan error: %w", err)
+	}
+
+	taskDomain := domain.NewTask(
+		taskModel.ID,
+		taskModel.Version,
+		taskModel.Title,
+		taskModel.Description,
+		taskModel.Completed,
+		taskModel.Created_At,
+		taskModel.Completed_At,
+		taskModel.AuthorUserID,
+	)
+
+	return taskDomain, nil
+}
