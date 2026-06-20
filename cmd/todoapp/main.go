c9181d06fb8ac7bf097d0d201cb3dfe7299d09c3
raizonw <i.dmitriev781@gmail.com>
2026-06-20 14:11:56 +0300

    added logs-cleanup makefile target

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -6,7 +6,9 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	core_config "github.com/raizonw/todo-app/internal/core/config"
 	core_logger "github.com/raizonw/todo-app/internal/core/logger"
 	core_postgres_pool "github.com/raizonw/todo-app/internal/core/repository/postgres/pool"
 	core_http_middleware "github.com/raizonw/todo-app/internal/core/transport/http/middleware"
@@ -21,6 +23,8 @@ import (
 )
 
 func main() {
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -34,6 +38,8 @@ func main() {
 		os.Exit(1)
 	}
 	defer logger.Close()
+
+	logger.Debug("application time zone", zap.Any("zone", time.Local))
 
 	logger.Debug("initializing postgres connection pool")
 	pool, err := core_postgres_pool.NewConnectionPool(
